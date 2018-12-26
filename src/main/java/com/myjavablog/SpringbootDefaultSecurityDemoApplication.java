d9a5e6763b99a24efcf8144a1266ee24f132c428
anupb <anup.bhagwat7@gmail.com>
2018-12-26 16:43:04 +0530

    Spring boot security with default authentication

---
+++ src/main/java/com/myjavablog/SpringbootDefaultSecurityDemoApplication.java
@@ -0,0 +1,15 @@
+package com.myjavablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDefaultSecurityDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootDefaultSecurityDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/myjavablog/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.myjavablog.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController implements ErrorController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "Successfully logged in...";
+    }
+
+    final String PATH = "/error";
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
